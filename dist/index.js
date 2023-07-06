65f293f825c72e22db17fb56e00622dfa2a2bfe8
Jposeph99 <joseph.ahmed65@yahoo.com>
2023-07-06 15:15:10 -0600

    home work done

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,28 @@
 "use strict";
-console.log('helllo');
-let age = 70;
-console.log(age);
-let age2 = 55;
-let name2 = ' josh';
-let info = age2 + name2;
-console.log(info);
+function reverseList(list) {
+    return list.reverse();
+}
+console.log(reverseList([1, 2, 3, 4]));
+function getChar(c) {
+    return String.fromCharCode(c);
+}
+console.log(getChar(55));
+let a = 118;
+console.log(a);
+let b = 'Thieves';
+console.log(b);
+let c = [true, false, false];
+console.log(c);
+let e = [3];
+let f;
+console.log(f);
+let g = [];
+let song = {
+    title: 'Lose Yourself'
+};
+let prices = ['100', 200, 300];
+prices[0] = '$100';
+function myFunc(a, b) {
+    return a + b;
+}
+console.log(myFunc(5, 6));
